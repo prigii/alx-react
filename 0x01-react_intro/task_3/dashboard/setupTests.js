ef57b421b05cca2614576dc191ed485e995cf089
Peter Rigii <theonlyrigii@gmail.com>
2024-01-31 21:45:44 +0300

    installed create-react-app and run it

--- 0x01-react_intro/task_3/dashboard/setupTests.js
+++
@@ -1,6 +0,0 @@
-// src/setupTests.js
-
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
-
-configure({ adapter: new Adapter() });
--- 0x01-react_intro/task_5/dashboard/src/App/index.js
+++
@@ -1,6 +0,0 @@
-// src/index.js
-import './style.css';
-import { getFullYear, getFooterCopy } from '../utils';
-import logo from '../assets/images/holberton-logo.png';
-
-console.log('Hello from Webpack!');
