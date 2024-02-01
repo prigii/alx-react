64c87d5ebcb48ebc5dde40baea74276a40d7aca4
Peter Rigii <theonlyrigii@gmail.com>
2024-02-01 18:47:41 +0300

    Dev servers, modules, and tree shaking

---
+++ 0x00-Webpack/task_3/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require("path");
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    main: path.resolve(__dirname, './js/dashboard_main.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: 'bundle.js',
+  },
+  devServer: {
+    contentBase: './deploy',
+    open: true
+  },
+};
