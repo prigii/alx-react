ecad3fbb4644bf4f38508b3e65167dc6f447706c
Peter Rigii <theonlyrigii@gmail.com>
2024-02-02 15:22:07 +0300

    updated files

--- 0x01-react_intro/task_5/dashboard/src/App.js
+++ 0x01-react_intro/task_5/dashboard/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+import logo from './holberton-logo.jpg';
 import './App.css';
+import { getFooterCopy, getFullYear } from './utilis';
+import Notifications from './Notifications';
 
 function App() {
   return (
     <div className="App">
+       <Notifications />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>School dashboard</h1>
       </header>
+      <body className="App-body">
+        <p>Login to access the full dashboard</p>
+
+        <form>
+
+          {/* Email */}
+          <div>
+            <label htmlFor="email">Email: </label>
+              <input type="email" id="email" />            
+          </div>
+
+          {/* Password Input */}
+          <div>
+            <label htmlFor="password">Password: </label>
+            <input type="password" id="password" />
+          </div>
+
+          {/* OK Button */}
+          <button>OK</button>
+        </form>
+      </body>
+      <footer className="App-footer">
+        <p>{`${getFooterCopy(true)} - ${getFullYear()}`}</p>
+      </footer>
     </div>
   );
 }
--- 0x01-react_intro/task_5/dashboard/src/App.test.js
+++ 0x01-react_intro/task_5/dashboard/src/App.test.js
@@ -1,8 +1,25 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { shallow } from 'enzyme';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App Component', () => {
+  it('renders without crashing', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-header', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-header').exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-body', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-body').exists()).toBe(true);
+  });
+
+  it('renders a div with the class App-footer', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('.App-footer').exists()).toBe(true);
+  });
 });
