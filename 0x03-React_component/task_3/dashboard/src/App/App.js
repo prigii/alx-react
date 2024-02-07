ed85673eadddf4a330693368c3ac462538c77092
Peter Rigii <theonlyrigii@gmail.com>
2024-02-07 12:22:25 +0300

    Use the new components

--- 0x03-React_component/task_3/dashboard/src/App/App.js
+++ 0x03-React_component/task_3/dashboard/src/App/App.js
@@ -43,7 +43,21 @@ class App extends React.Component {
             <Notifications listNotifications={this.listNotifications} />
             <Header />
           </div>
-          {this.props.isLoggedIn ? <CourseList listCourses={this.listCourses} /> : <Login />}
+          {this.props.isLoggedIn ? (
+            <BodySectionWithMarginBottom title="Course list">
+              <CourseList listCourses={this.listCourses} />
+            </BodySectionWithMarginBottom>
+          ) : (
+            <BodySectionWithMarginBottom title="Log in to continue">
+              <Login />
+            </BodySectionWithMarginBottom>
+          )}
+          <BodySection title="News from the school">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis at tempora odio, necessitatibus repudiandae reiciendis cum nemo sed asperiores ut molestiae eaque aliquam illo ipsa
+              iste vero dolor voluptates.
+            </p>
+          </BodySection>
           <Footer />
         </div>
       </React.Fragment>
