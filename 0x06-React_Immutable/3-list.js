9c557a0b6d639e245b364b50210a1a086445e34d
Peter Rigii <theonlyrigii@gmail.com>
2024-02-21 16:34:10 +0300

    semistandard fix

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,6 +1,6 @@
 import { List } from 'immutable';
 
-export function getListObject (array) {
+export function getListObject(array) {
   return List(array);
 }
 
--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,6 +1,6 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents (object) {
+export default function printBestStudents(object) {
   const seq = Seq(object);
 
   const filtered = seq.filter((student) => {
