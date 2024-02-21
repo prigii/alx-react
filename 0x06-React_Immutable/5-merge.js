376a74c40a5e99b369e1213222fd3c314761c9d4
Peter Rigii <theonlyrigii@gmail.com>
2024-02-21 15:11:41 +0300

    Merge & concat

--- 0x06-React_Immutable/5-merge.js
+++ 0x06-React_Immutable/5-merge.js
@@ -1,13 +1,8 @@
 import { List, Map } from 'immutable';
 
-export default function concatElements(page1, page2) {
-    // Concatenate the two arrays and convert them into a List
-    return List(page1.concat(page2));
-}
+export const concatElements = (page1, page2) => List([...page1, ...page2]);
 
-export default function mergeElements(page1, page2) {
-    // Merge the two objects and convert them into a Map
-    const mergedMap = Map(page1).merge(Map(page2));
-    // Convert the Map values into a List
+export const mergeElements = (page1, page2) => {
+    const mergedMap = Map({...page1, ...page2});
     return List(mergedMap.toList());
-}
+};
