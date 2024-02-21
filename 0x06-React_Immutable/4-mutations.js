dd17888193be1535e842a78b99d8da02fda8e07c
Peter Rigii <theonlyrigii@gmail.com>
2024-02-21 15:00:10 +0300

    Chained mutations

---
+++ 0x06-React_Immutable/4-mutations.js
@@ -0,0 +1,18 @@
+const { Map } = require('immutable');
+
+// Create the initial Immutable Map
+const map = Map({
+    1: 'Liam',
+    2: 'Noah',
+    3: 'Elijah',
+    4: 'Oliver',
+    5: 'Jacob',
+    6: 'Lucas'
+});
+
+// Modify values in the initial map to create map2
+const map2 = map.withMutations(mutableMap => {
+    mutableMap.set(2, 'Benjamin').set(4, 'Oliver');
+});
+
+export { map, map2 };
