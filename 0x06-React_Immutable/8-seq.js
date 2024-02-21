b69e529c28700d5044b9d1e0a262152a90788417
Peter Rigii <theonlyrigii@gmail.com>
2024-02-21 15:56:18 +0300

    Lazy Seq

--- 0x06-React_Immutable/8-seq.js
+++ 0x06-React_Immutable/8-seq.js
@@ -1,18 +1,24 @@
 import { Seq } from 'immutable';
 
-export default function printBestStudents(grades) {
-    const filteredGrades = Seq(grades)
-        .filter(student => student.score >= 70)
-        .map(student => ({
-            ...student,
-            firstName: student.firstName.charAt(0).toUpperCase() + student.firstName.slice(1),
-            lastName: student.lastName.charAt(0).toUpperCase() + student.lastName.slice(1)
-        }))
-        .toObject();
+export default function printBestStudents(object) {
+  const seq = Seq(object);
 
-    const formattedGrades = JSON.stringify(filteredGrades)
-        .replace(/"(\w+)":/g, '$1:')
-        .replace(/"(\d+)":/g, '"$1":');
+  const filtered = seq.filter((student) => {
+    student.firstName.charAt(0).toUpperCase();
+    return student.score > 70;
+  });
 
-    console.log(formattedGrades);
+  function capFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  const JSObject = filtered.toJS();
+
+  Object.keys(JSObject).map((key) => {
+    JSObject[key].firstName = capFirstLetter(JSObject[key].firstName);
+    JSObject[key].lastName = capFirstLetter(JSObject[key].lastName);
+    return JSObject[key];
+  });
+
+  console.log(JSObject);
 }
