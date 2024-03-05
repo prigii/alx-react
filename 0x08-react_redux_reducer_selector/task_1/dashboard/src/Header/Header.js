39ea20f62f2ebc8003d42cbc67c635934ae1f75a
Peter Rigii <theonlyrigii@gmail.com>
2024-03-05 15:41:14 +0300

    Use Immutable for the UI Reducer

---
+++ 0x08-react_redux_reducer_selector/task_1/dashboard/src/Header/Header.js
@@ -0,0 +1,45 @@
+import React from "react";
+import logo from "../assets/holberton-logo.jpg";
+import { StyleSheet, css} from 'aphrodite';
+import { ContextType } from "../App/AppContext";
+
+const styles = StyleSheet.create({
+  'App-header': {
+    fontSize: '1.4rem',
+    color: '#e0354b',
+    display: 'flex',
+    alignItems: 'center',
+    borderBottom: '3px solid #e0354b',
+  },
+    
+    'img': {
+      width: '200px',
+      height: '200px',
+    },
+});
+
+
+function Header() {
+  const { user, logOut } = useContext(AppContext);
+
+  return (
+    <>
+      <div className={css(styles['App-header'])}>
+        <img src={logo} className={css(styles.img)} alt="logo" />
+        <h1>School dashboard</h1>
+      </div>
+      {user.isLoggedIn && (
+        <section className={css(styles.greeting)} id="logoutSection">
+          Welcome<strong> {user.email} </strong>
+          <em>
+            <a href="#" onClick={logOut}>
+              (logout)
+            </a>
+          </em>
+        </section>
+      )}
+    </>
+  );
+}
+
+export default Header;
