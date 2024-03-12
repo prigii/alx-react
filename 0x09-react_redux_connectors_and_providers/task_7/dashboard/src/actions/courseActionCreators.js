ff6991da66b4194e4a3aea97643d08f02fe5ab25
Peter Rigii <theonlyrigii@gmail.com>
2024-03-12 16:05:19 +0300

    Connect courses: create a fetch courses function

--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.js
@@ -1,18 +1,41 @@
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import {
+  SELECT_COURSE,
+  UNSELECT_COURSE,
+  FETCH_COURSE_SUCCESS,
+} from "./courseActionTypes";
 
-export function selectCourse(index) {
+import "node-fetch";
+
+export const selectCourse = (index) => {
   return {
     type: SELECT_COURSE,
-    index: index,
+    index,
   };
-}
+};
 
-export const boundselectCourse = (index) => dispatch(selectCourse(index));
+export const boundSelectCourse = (index) => dispatch(selectCourse(index));
 
-export function unSelectCourse(index) {
+export const unSelectCourse = (index) => {
   return {
     type: UNSELECT_COURSE,
-    index: index,
+    index,
   };
-}
-export const boundunSelectCourse = (index) => dispatch(unSelectCourse(index));
+};
+
+export const boundUnSelectCourse = (index) => dispatch(unSelectCourse(index));
+
+export const setCourses = (data) => {
+  return {
+    type: FETCH_COURSE_SUCCESS,
+    data,
+  };
+};
+
+export const fetchCourses = () => {
+  return (dispatch) => {
+    return fetch("./courses.json")
+      .then((res) => res.json())
+      .then((data) => dispatch(setCourses(data)))
+      .catch((error) => {});
+  };
+};
--- 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
+++ 0x09-react_redux_connectors_and_providers/task_7/dashboard/src/actions/courseActionCreators.test.js
@@ -1,11 +1,48 @@
-import { selectCourse, unSelectCourse } from "./courseActionCreators";
-import { SELECT_COURSE, UNSELECT_COURSE } from "./courseActionTypes";
+import {
+  selectCourse,
+  unSelectCourse,
+  setCourses,
+  fetchCourses,
+} from "./courseActionCreators";
 
-describe("tests for action creators", () => {
-  it("should return right action payload and type when selectCourse is called", () => {
-    expect(selectCourse(1)).toEqual({ type: SELECT_COURSE, index: 1 });
+import {
+  SELECT_COURSE,
+  UNSELECT_COURSE,
+  FETCH_COURSE_SUCCESS,
+} from "./courseActionTypes";
+
+import fetchMock from "fetch-mock";
+
+import configureStore from "redux-mock-store";
+import thunk from "redux-thunk";
+const middlewares = [thunk];
+const mockStore = configureStore(middlewares);
+
+describe("action creators tests", function () {
+  it("selectCourse should return: { type: SELECT_COURSE, index: 1 }", function () {
+    const result = selectCourse(1);
+
+    expect(result).toEqual({ type: SELECT_COURSE, index: 1 });
   });
-  it("should return right action payload and type when unSelectCourse is called", () => {
-    expect(unSelectCourse(1)).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  it("unSelectCourse should return: { type: UNSELECT_COURSE, index: 1 }", function () {
+    const result = unSelectCourse(1);
+
+    expect(result).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  });
+  it("unSelectCourse should return: { type: UNSELECT_COURSE, index: 1 }", function () {
+    const result = unSelectCourse(1);
+
+    expect(result).toEqual({ type: UNSELECT_COURSE, index: 1 });
+  });
+  it("verify that the fetch is working correctly", function () {
+    const store = mockStore({});
+    fetchMock.restore();
+
+    fetchMock.get("./courses.json", "{}");
+
+    return store.dispatch(fetchCourses()).then(() => {
+      const actions = store.getActions();
+      expect(actions[0]).toEqual(setCourses({}));
+    });
   });
 });
