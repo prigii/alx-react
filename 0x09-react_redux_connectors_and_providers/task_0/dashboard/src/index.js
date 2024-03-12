94cc0e47629352cd93e963027add0d36be9291e8
Peter Rigii <theonlyrigii@gmail.com>
2024-03-12 13:03:54 +0300

    Create a small store

--- 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
+++ 0x09-react_redux_connectors_and_providers/task_0/dashboard/src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App/App';
+import { Provider } from 'react-redux';
+import { Map } from 'immutable'
+import uiReducer, { initialState } from './reducer'
+import { createStore } from 'redux'
 
+const store = createStore(uiReducer, Map(initialState))
+
+export default store
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <>
     <React.StrictMode>
-      <App />
-    </React.StrictMode>    
-  </>
+      <Provider store={store}>
+          <App />
+      </Provider>
+    </React.StrictMode>,    
+  document.getElementById('root')
 );
